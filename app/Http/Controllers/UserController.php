0c770ede48641226eb066ab95b737433c98f501c
aryan zuda firdaus <aryanzf1@gmail.com>
2026-03-10 00:03:39 +0700

    eloquent ORM

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function profile($id,$name){
-        return view('user.profile',[
-            'id'=>$id,
-            'name'=>$name
-        ]);
+    public function index()
+    {
+        // tambah data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data); // update data user
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
     }
+    // public function profile($id,$name){
+    //     return view('user.profile',[
+    //         'id'=>$id,
+    //         'name'=>$name
+    //     ]);
+    // }
 }
