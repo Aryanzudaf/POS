f0ceec3e32610dd86593ffab9bb7f9a97c695cbc
aryan zuda firdaus <aryanzf1@gmail.com>
2026-03-09 23:39:22 +0700

    query builder

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalesController;
+use App\Http\Controllers\KategoriController;
 
 Route::get('/', [HomeController::class,'index']);
 
@@ -31,3 +32,5 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+
+Route::get('/kategori', [KategoriController::class, 'index']);
